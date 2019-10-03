c7267de612446c93cc7e3c2190e473aaeb245b34
jebeni777 <46410301+jebeni777@users.noreply.github.com>
2019-10-03 15:47:49 -0700

    display latitude and longitude and map of location

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+let styles = {
+  listStyle: 'none'
+};
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      address: '',
+      Latitude: '',
+      Longitude: '',
+      map: '',
+      readyToRender: false
+    };
+
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    let userData = this.state
+    axios.post('http://www.mapquestapi.com/geocoding/v1/address?key=7lOkL4uZoyNEeYOhPhl03PNPrhM5O714',
+      { location: userData.address })
+      .then((response) => {
+        console.log(response)
+        let readyToDisplayLat = response.data.results[0].locations[0].latLng.lat;
+        let readyToDisplayLng = response.data.results[0].locations[0].latLng.lng;
+        let mapDisplay = response.data.results[0].locations[0].mapUrl
+        this.setState({
+          Latitude: readyToDisplayLat,
+          Longitude: readyToDisplayLng,
+          map: mapDisplay,
+          readyToRender: true
+        })
+      });
+  }
+
+  onChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.onSubmit}>
+          <input
+            type="text"
+            name={"address"}
+            value={this.state.address}
+            placeholder={"Enter address"}
+            onChange={this.onChange}
+          />{" "}
+          <input type="submit" value="Submit" />
+        </form>
+        {this.state.readyToRender &&
+          <ul style={styles}>
+            <li>{this.state.Latitude}</li>
+            <li>{this.state.Longitude}</li>
+            <img src={this.state.map} alt="mapquest" />
+          </ul>}
+      </div>
+    );
+  }
 }
 
-export default App;
